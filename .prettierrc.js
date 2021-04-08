ceaa88969bd2b144f14fe3929818190f3436a1eb
YifanWang <15868163725@163.com>
2021-04-08 17:55:08 +0800

    feature: 初始化

---
+++ .prettierrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  printWidth: 80,
+  trailingComma: 'es5',
+  tabWidth: 2,
+  semi: true, // 强制代码结尾使用分号(默认)
+  singleQuote: true, //使用单引号替代双引号
+  htmlWhitespaceSensitivity: 'ignore', // 防止包裹文字时候结束标签的结尾尖括号掉到了下一行
+};
